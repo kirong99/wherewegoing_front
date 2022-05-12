c627025b06759c244c99a0cbcb8fd6f0efde42bb
giryong <giryong99@naver.com>
2022-05-12 16:53:03 +0900

    [0512] 페이지 전체 통합

--- js/script.js
+++ js/script.js
@@ -1,4 +1,47 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var calendarEl = document.getElementById('calendar');
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+        height: 600,
+        contentHeight: 600,
+        initialView: 'dayGridMonth',
+        editable: true,
+        selectable: true,
+        nowIndicator: true,
+        locale: 'ko',
+        eventAdd: function (obj) {
+            console.log(obj);
+        },
+        eventChange: function (obj) {
+            console.log(obj);
+        },
+        eventRemove: function (obj) {
+            console.log(obj);
+        },
+        events: [
+            {
+                title: 'event1',
+                start: '2022-05-02'
+            },
+            {
+                title: 'event2',
+                start: '2022-05-07',
+                end: '2022-05-08'
+            },
+            {
+                title: 'event3',
+                start: '2022-05-23'
+            }
+        ]
+    })
+    calendar.render();
+});
+
+calendar.forEach(function(element){
+    console.log(element);
+});
+
 $(document).ready(function(){
+
     $(".course-select-box h3").click(function(){
         $(".course-select-box h3, .course-select-box ul").removeClass("on");
         $(this).addClass("on");
@@ -19,4 +62,12 @@ $(document).ready(function(){
     });
 
     document.getElementById('currentDate').value = new Date().toISOString().substring(0, 10);
+
+    $(".theme .theme_list>div").click(function(){
+        $(".theme .theme_list>div, .theme .theme_list ul").removeClass("on");
+        $(this).addClass("on");
+        $(this).next("ul").addClass("on");
+    });
+
 });
+
