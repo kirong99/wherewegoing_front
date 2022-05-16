6f0264d7565110f5a44413d78a0ae459ace7b509
giryong <giryong99@naver.com>
2022-05-16 23:03:40 +0900

    [0516] 로그인 알림 정비

--- login/login.js
+++ login/login.js
@@ -5,15 +5,12 @@ document.querySelector("#login").addEventListener("click", () => {
     if(id == document.querySelector("#id").value) {
         if(password == document.querySelector("#password").value) {
             alert("환영합니다!");
-            //document.write("환영합니다!");
         }
         else {
             alert("비밀번호가 맞지 않습니다.");
-            //document.write("비밀번호가 맞지 않습니다.");
         }
     }
     else {
         alert("아이디 혹은 비밀번호가 맞지 않습니다.");
-        //document.wrtie("아이디 혹은 비밀번호가 맞지 않습니다.");
     }
 });
